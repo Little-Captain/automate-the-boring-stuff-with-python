689f99ce67c5ee3b2461c591528483f170c4a65f
Little-Captain <coder.littlecaptain@gmail.com>
2019-11-05 12:45:29 +0800

    06.str completed

---
+++ 06.str/05.practice.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+def printTable(table):
+    colWidths = [len(max(row, key=lambda x: len(x))) for row in table]
+    for j in range(len(table[0])):
+        for i in range(len(table)):
+            print(table[i][j].rjust(colWidths[i]), end=' ')
+        print()
+
+
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+printTable(tableData)
