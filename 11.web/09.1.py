d90d1bf7d9e975e9ba5d329ff245568fe033dbfc
Little-Captain <coder.littlecaptain@gmail.com>
2019-11-25 15:38:23 +0800

    通过 sqlite 获取账号密码，然后解密获取真实密码

--- 11.web/09.1.py
+++ 11.web/09.1.py
@@ -6,3 +6,25 @@
 # 发送到提供的地址（你也许希望为这个程序建立一个独立的邮件账号）
 # 这是为程序添加通知功能的一种好方法
 # 你也可以编写类似的程序，从 Facebook 或 Twitter 账号发送消息
+
+# 从 sqlite 中获取邮箱用户的密码密码
+import sqlite3
+import os
+import sys
+
+connect = sqlite3.connect(r'C:\Users\Little-Captain\.sqlite\lc.db')
+cursor = connect.cursor()
+
+username = '1203118412'
+cursor.execute("select password from user where type = 'qq-mail' and username = %r" % username)
+values = cursor.fetchall()
+password = None
+if len(values) == 1 and len(values[0]) == 1:
+    password = os.popen('powershell encrypt -d %r' % values[0][0]).read()
+
+if password == None:
+    sys.exit(-1)
+
+print(password)
+
+# 
