5e75105ad08743d5c05327221af6a9951cf6bdd4
Little-Captain <coder.littlecaptain@gmail.com>
2019-12-10 21:31:41 +0800

    13.pdf completed

--- 13.pdf/11.practice.3.py
+++ 13.pdf/11.practice.3.py
@@ -13,3 +13,41 @@
 # 你应该尝试每个单词的大小写形式(在我的笔记本上，遍历来自字典文件的
 # 所有 88000 个大小写单词，只要几分钟时间。这就是不应该使用简单英语
 # 单词作为口令的原因)
+
+import PyPDF2
+
+'''
+# 加密
+pdfFileObj = open('./ratatePage.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+pdfWriter = PyPDF2.PdfFileWriter()
+pdfWriter.encrypt('word')
+for page in range(pdfReader.numPages):
+    pdfWriter.addPage(pdfReader.getPage(page))
+pdfOutput = open('./ratatePage_en.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+# 读入的文件对象, 写入的文件对象: 都必须在文件写入完成后才能关闭, 不然会出现问题
+# 文件打开了，就必须关闭，不管是读还是写
+pdfFileObj.close()
+pdfOutput.close()
+'''
+
+# 读取单词到一个 list
+dictFileObj = open('./dictionary.txt', 'r')
+words = list(map(lambda l: l.strip(), dictFileObj.readlines()))
+
+# 读取 pdf 进行解密
+pdfFileObj = open('./ratatePage_en.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+for w in words:
+    if pdfReader.decrypt(w.lower()) != 0:
+        print('密码为: %r' % w.lower())
+        break
+    elif pdfReader.decrypt(w.upper()) != 0:
+        print('密码为: %r' % w.upper())
+        break
+    else:
+        print(w)
+        continue
+
+pdfFileObj.close()
