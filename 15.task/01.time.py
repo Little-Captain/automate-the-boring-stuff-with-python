14c8edaefe9630d99fffa72975098e2b92e42308
Little-Captain <coder.littlecaptain@gmail.com>
2019-12-17 12:49:03 +0800

    15.2 completed

--- 15.task/01.time.py
+++ 15.task/01.time.py
@@ -8,6 +8,7 @@
 # time.time() 函数返回自那一刻以来的秒数，是一个浮点值
 import time
 
+'''
 # 返回值是 Unix 纪元的那一刻与 time.time() 被调用的那一刻之间的秒数
 print(time.time())
 
@@ -15,12 +16,14 @@ print(time.time())
 # 如果在代码块开始时调用 time.time()，并在结束时再次调用
 # 就可以用第二个时间戳减去第一个，得到这两次调用之间经过的时间。
 
+
 def calcProd():
     # Calculate the product of the first 100,000 numbers.
     product = 1
     for i in range(1, 10000000):
         product += 1
     return product
+
 
 startTime = time.time()
 prod = calcProd()
@@ -33,3 +36,36 @@ print('Took %s seconds to calculate.' % (endTime - startTime))
 # cProfile 是一个模块
 
 # time.sleep() 函数
+
+# 如果需要让程序暂停一下，就调用 time.sleep() 函数，并传入希望程序暂停的秒数
+for i in range(3):
+    print('Tick')
+    time.sleep(1)
+    print('Tock')
+    time.sleep(1)
+
+# time.sleep() 函数将阻塞(也就是说，它不会返回或让程序执行其他代码)
+# 直到传递给 time.sleep() 的秒数流逝
+
+# 暂停 30 秒，推荐写法
+# 这样可以 Ctrl-C 快速中断
+# 如果在这 30 秒内的某个时候按 Ctrl-C，应该马上看到抛出 KeyboardInterrupt 异常
+for i in range(30):
+    time.sleep(1)
+'''
+
+# 数字四舍五入
+# 用 Python 内置的 round() 函数按照指定的精度四舍五入到一个浮点数
+# 只要传入要舍入的数字，再加上可选的第二个参数，指明需要传入到小数点后多少位
+# 如果省略第二个参数，round() 将数字四舍五入到最接近的整数
+now = time.time()
+print(now)
+print(round(now))
+print(round(now, 2))
+print(round(now, 4))
+print(type(now))
+print(type(round(now)))
+print(type(round(now, 2)))
+print(type(round(now, 4)))
+# 有小数点的数，就是 float
+# 没有小数点的数，就是 int
